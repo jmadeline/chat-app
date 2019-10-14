c19f694ae38ec9dbb2e61135e77fd2a223f325af
Madeline Joubert <madelinejoubert44@gmail.com>
2019-10-14 11:52:37 +0200

    Add onClick action on the status indicator

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -2,22 +2,35 @@ import React from "react";
 import './Contact.css';
 
 
-function Contact(props) {
-  return (
-    <div className="Contact">
-      <img className="avatar" src={props.avatar} alt={props.name}></img>
-      <div className="name">
-        {props.name}
-        <div className="status">
-          <div className="status-text">
-            <div className={props.status ? "status-online" : "status-offline"}>
+class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      online: props.status
+    };
+  }
+
+  render() {
+    return (
+      <div className="Contact">
+        <img className="avatar" src={this.props.avatar} alt={this.props.name}></img>
+        <div className="name">
+          {this.props.name}
+          <div className="status">
+            <div className="status-text">
+              <div className={this.state.online ? "status-online" : "status-offline"} onClick={event => {
+                const newStatus = !this.state.online;
+                this.setState({ online: newStatus });
+              }}>
+              </div>
+              {this.state.online ? "online" : "offline"}
             </div>
-            {props.status ? "online" : "offline"}
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
+
 
 export default Contact;
